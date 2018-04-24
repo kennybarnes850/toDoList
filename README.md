02b2cd836c92a00febdb62f4d08f7f1e201eb9c0
Kenneth Barnes <kennethbarnes@Kenneths-MacBook-Air.local>
2018-04-24 06:03:07 -0400

    second commit-added delete Todo function

--- README.md
+++ README.md
@@ -1,2 +1,6 @@
 # toDoList
 Todo List created with HTML and Javascript
+
+First encounter with splice function to delete Todos
+
+
--- list.js
+++ list.js
@@ -5,12 +5,24 @@ var input = prompt("What would you like to do?");
 while(input !== "quit"){
     //handle input
     if(input === "list"){
-        console.log(todos);
+        console.log("**********")
+        todos.forEach(function(todo, index){
+        console.log(index + ": " + todo);
+        });
+        console.log("**********")
     } else if(input === "new"){
         //ask user for a new todo
         var newTodo = prompt("Enter new todo");
         //add to todos array
         todos.push(newTodo);
+        console.log("Added Todo");
+    }else if(input === "delete"){
+        //ask for undex of Todo to be deleted
+        var index = prompt("Enter index of todo to delete");
+        //delete that Todo 
+        //I get introduced to splice =) it allows you to choose from the index. in this case I want to be able to choose a Todo from my list to delete
+        todos.splice(index,1);
+        console.log("Deleted Todo");
     }
     //ask again for new input
     var input = prompt("What would you like to do?");
